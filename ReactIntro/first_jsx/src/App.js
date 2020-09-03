8d768abb20067e62050db95a8f25ce92b5b83d52
a-garey <agarey@uchicago.edu>
2020-09-03 16:40:54 -0400

    React intro
    React assignments

---
+++ ReactIntro/first_jsx/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Hello Dojo!</h1>
+    <h2>Things I Need to Do:</h2>
+    <ul>
+      <li>
+        <h4>Learn React</h4>
+      </li>
+      <li>
+        <h4>Climb Mt. Everest</h4>
+      </li>
+      <li>
+        <h4>Run a marathon</h4>
+      </li>
+      <li>
+        <h4>Feed the dogs</h4>
+      </li>
+    </ul>
+    </>
+  );
+}
+
+export default App;
