e354e82eaacf6d8c856254487346a1a06754b763
a-garey <agarey@uchicago.edu>
2020-09-10 16:48:53 -0400

    Update Form.js

--- ReactIntro/hook_forms/src/components/Form.js
+++ ReactIntro/hook_forms/src/components/Form.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = props => {
 
     const {input, setInput} = props;
+    const [inputError, setInputError] = useState("");
 
     const onChange = e => {
         setInput({
             ...input,
-            [e.target.name]: e.target.value
-
+            [e.target.name]: e.target.value,
         });
-
+        if(e.target.value.firstName < 1) {
+            setInputError("First name is required!");
+        } else if(e.target.value.firstName < 3) {
+            setInputError("First name must be 3 characters or longer!");
+        }
+        if(e.target.value.lastName < 1) {
+            setInputError("Last name is required!");
+        } else if(e.target.value.lastName < 3) {
+            setInputError("Last name must be 3 characters or longer!");
+        }
+        if(e.target.value.email < 5) {
+            setInputError("Email address must be at least 5 charachters");
+        }
+        if(e.target.value.password < 8 || e.target.value.confirmPassword != e.target.value.password) {
+            setInputError("Invalid credentials");
+        }
     };
 
     return (
